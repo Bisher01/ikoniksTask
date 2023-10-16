d15e05b8ea8937708796dbf7d0953aa1d6548e82
Bisher hasani <bisher.alhasani@ixcoders.com>
2023-10-16 21:31:49 +0300

    unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:task/dependency_injection.dart';
+import 'package:task/features/devices/presentation/pages/search_devices_screen.dart';
+import 'package:task/features/splash/presentation/splash_screen.dart';
 import 'package:task/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  testWidgets(
+    'expect splash screen when we open the app',
+    (WidgetTester tester) async {
+      await inject();
+      await tester.runAsync(
+        () async {
+          await tester.pumpWidget(const MyApp());
+          expect(find.byType(SplashScreen), findsOneWidget);
+          expect(find.byType(SearchDevicesScreen), findsNothing);
+        },
+      );
+    },
+  );
 }
